ea75f25c8088d22ab4ccb18d4ce719c7c9090a63
dhave-dev13 <dhave.dev13@gmail.com>
2026-04-18 20:11:31 +0800

    app | add env variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:finderapp/core/config/app_config.dart';
 import 'package:finderapp/features/tracker/presentation/bloc/geolocator_bloc/geolocator_bloc.dart';
+import 'package:finderapp/features/tracker/presentation/bloc/target_location/target_location_bloc.dart';
 import 'package:finderapp/features/tracker/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+
+  await AppConfig(
+      baseUrl: 'https://api.mockapi.com/api/v1',
+      dotenvFile: '.env.dev',
+    ).init();
+
   runApp(const MainApp());
 }
 
@@ -15,6 +23,7 @@ class MainApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<GeolocatorBloc>(create: (context) => GeolocatorBloc()),
+        BlocProvider<TargetLocationBloc>(create: (context) => TargetLocationBloc()),
       ],
       child: MaterialApp(home: Scaffold(body: HomePage())),
     );
