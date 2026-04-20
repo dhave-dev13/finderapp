75be7c43878c40109a94cbbb96838d0c15d6c27a
dhave-dev13 <dhave.dev13@gmail.com>
2026-04-20 16:51:45 +0800

    app | set android permissions for location

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.finderapp"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
